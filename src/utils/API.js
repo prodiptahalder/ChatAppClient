5438b7955b7f1a8410b64a47dcf4a4d9e5002066
Prodipta <haldip9500@gmail.com>
2022-06-27 22:52:28 +0530

    server changed

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,4 +1,4 @@
-const host = `${process.env.SERVER}/api/v1/`;
+const host = `http://192.168.56.1:3001/api/v1/`;
 export const registerRoute = `${host}users/register`;
 export const loginRoute = `${host}users/login`;
 export const allUsersRoute = `${host}users/allUsers`;
